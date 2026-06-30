633b88b61c216322f5ffc103bce1afc6873e1dc2
Nadia <nadiachouiref@gmail.com>
2026-06-30 23:23:11 +0200

    mise en place du routage avec React Router

---
+++ src/pages/Error.jsx
@@ -0,0 +1,9 @@
+function Error() {
+  return (
+    <div>
+      <h1>404 - Page non trouvée</h1>
+    </div>
+  )
+}
+
+export default Error
---
+++ src/pages/Property.jsx
@@ -0,0 +1,9 @@
+function Property() {
+  return (
+    <div>
+      <h1>Page Logement</h1>
+    </div>
+  )
+}
+
+export default Property
